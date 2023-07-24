098ffd55901c3c7c50809ce9e0cfc1605ebd4e56
shahzain46 <shahzain6016@gmail.com>
2023-07-24 12:53:51 -0700

    login form
    login and signup form and api through node.js

---
+++ src/mongodb.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://shahzain:12shah34zain@mydatabase.lboguaf.mongodb.net/test')
+.then(()=>{
+console.log("connected to database")
+})
+.catch(()=>{
+console.log("connection fail")
+})
+
+
+const logInSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    }
+})
+
+
+const collection = new mongoose.model('collection1',logInSchema)
+
+module.exports=collection
+ 
